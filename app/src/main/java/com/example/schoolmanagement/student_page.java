fe4469f7159646706a1166eb5daf550c1819e47a
BrianaNzivu <bjnzivu@gmail.com>
2023-12-05 09:32:55 +0300

    Got Firebase to work suckers!

---
+++ app/src/main/java/com/example/schoolmanagement/student_page.java
@@ -0,0 +1,59 @@
+package com.example.schoolmanagement;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.schoolmanagement.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class student_page extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    String fName, lName, sId;
+    FirebaseDatabase db;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_page);
+
+        EditText firstNameEditText = findViewById(R.id.first_name);
+        EditText lastNameEditText = findViewById(R.id.last_name);
+        EditText studentIdEditText = findViewById(R.id.student_id);
+        Button registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = firstNameEditText.getText().toString();
+                String lastName = lastNameEditText.getText().toString();
+                String studentId = studentIdEditText.getText().toString();
+
+                if (!firstName.isEmpty() && !lastName.isEmpty() && !studentId.isEmpty()) {
+                    Student_info users = new Student_info(firstName, lastName, studentId);
+                    db = FirebaseDatabase.getInstance();
+                    reference = db.getReference("Students");
+                    reference.child(firstName).setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            firstNameEditText.setText("");
+                            lastNameEditText.setText("");
+                            studentIdEditText.setText("");
+                            Toast.makeText(student_page.this, "Successfuly Updated", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
